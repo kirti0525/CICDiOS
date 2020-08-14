c5a3e41c20b3ae7c6597625036669fd5b4e0a00e
kirti0525 <kirtikalra61@gmail.com>
2020-08-14 16:19:52 +0530

    Initial commit

---
+++ RetirementCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RetirementCalculator
+//
+//  Created by Kirti Kalra on 14/08/20.
+//  Copyright © 2020 Kirti Kalra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
